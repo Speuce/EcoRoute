896827acc6b9ae50eb2a96ed162a1cb5bc2c0054
Kaedyn Waters <kwaters@smail.pembinatrails.ca>
2026-01-23 09:54:36 -0600

    Add Routes And Colour To Page

--- routes.js
+++ routes.js
@@ -11,5 +11,10 @@ window.routes = [
     {"from": "SFO", "to": "LAX", "co2": 100},
     {"from": "JFK", "to": "SFO", "co2": 550},
     {"from": "SFO", "to": "SEA", "co2": 120},
-    {"from": "SEA", "to": "LAX", "co2": 200}
+    {"from": "SEA", "to": "LAX", "co2": 200},
+    {"from": "WIN", "to": "SEA", "co2": 300}, //This and further are mine
+    {"from": "SEA", "to": "WIN", "co2": 300},
+    {"from": "ATL", "to": "WIN", "co2": 250},
+    {"from": "WIN", "to": "ATL", "co2": 250},
+    {"from": "WIN", "to": "ORD", "co2": 400}
 ]
