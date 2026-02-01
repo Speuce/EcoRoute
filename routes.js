25cbc4840183eecb731523711bb7c9a157020ca2
SneakySnookz <144716335+SneakySnookz@users.noreply.github.com>
2026-02-01 10:24:01 -0600

    Update routes.js

--- routes.js
+++ routes.js
@@ -11,7 +11,6 @@ window.routes = [
     {"from": "SFO", "to": "LAX", "co2": 100},
     {"from": "JFK", "to": "SFO", "co2": 550},
     {"from": "SFO", "to": "SEA", "co2": 120},
-    {"from": "SEA", "to": "LAX", "co2": 200},
     {"from": "WIN", "to": "SEA", "co2": 300}, //This and further are mine
     {"from": "SEA", "to": "WIN", "co2": 300},
     {"from": "ATL", "to": "WIN", "co2": 250},
